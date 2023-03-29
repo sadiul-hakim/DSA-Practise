749d658de273ef8661143a496613a3a8997c24f2
sadiul-hakim <sadiulhakim@gmail.com>
2023-03-29 22:06:06 +0600

    Java SE

---
+++ src/main/java/com/hakim/javase/JavaSE.java
@@ -0,0 +1,14 @@
+package com.hakim.javase;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Hakim
+ */
+public class JavaSE {
+
+    public static void main(String[] args) throws IOException {
+        
+    }
+}
